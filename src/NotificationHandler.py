0f15df954f20943c69060b60fe50434a794536ba
Fly-onlyone <172966293+Fly-onlyone@users.noreply.github.com>
2024-10-21 19:07:13 +0700

    chore: using dedicated html file

--- src/NotificationHandler.py
+++ src/NotificationHandler.py
@@ -1,66 +1,31 @@
 from datetime import datetime
-
 import apprise
-
+from jinja2 import Template
 
 def send_mission_data_via_email_html(todays_data):
-    # Get the current date in the format 'day/month/year'
     current_date = datetime.now().strftime("%d/%m/%Y")
 
     if todays_data['day'] != current_date:
         print(f"No mission data available for today ({current_date}).")
         return
 
-    # HTML Email body with inline CSS styles
-    mission_summary_html = f"""
-    <html>
-    <head>
-        <style>
-            body {{ font-family: Arial, sans-serif; }}
-            h1 {{ color: #4CAF50; }}
-            table {{ width: 100%; border-collapse: collapse; margin: 25px 0; }}
-            th, td {{ padding: 12px 15px; border: 1px solid #ddd; text-align: left; }}
-            th {{ background-color: #f2f2f2; }}
-            tr:nth-child(even) {{ background-color: #f9f9f9; }}
-        </style>
-    </head>
-    <body>
-        <h1>Mission Report for {todays_data['day']}</h1>
-        <p><strong>Check-in Status:</strong> {todays_data['check_in']}</p>
-        <h2>Mission Status:</h2>
-        <table>
-            <tr>
-                <th>Mission</th>
-                <th>Status</th>
-            </tr>
-    """
+    with open('./src/message/mission.html', 'r') as file:
+        template = Template(file.read())
 
-    for mission in todays_data['missions']:
-        mission_summary_html += f"""
-            <tr>
-                <td>{mission['name']}</td>
-                <td>{mission['state']}</td>
-            </tr>
-        """
+    mission_summary_html = template.render(
+        day=todays_data['day'],
+        check_in=todays_data['check_in'],
+        missions=todays_data['missions']
+    )
 
-    mission_summary_html += """
-        </table>
-    </body>
-    </html>
-    """
-
-    # Apprise instance
     apobj = apprise.Apprise()
-
-    # Add your email provider (make sure credentials are correct)
     apobj.add('mailto://galevan61:jlqrqxxtaggvnuce@gmail.com')
 
-    # Send the HTML-formatted email
     try:
         apobj.notify(
             body=mission_summary_html,
             title=f"Mission Report for {todays_data['day']}",
-            body_format='html',  # This tells Apprise to send HTML content
+            body_format='html',
         )
         print("Email sent successfully.")
     except Exception as e:
