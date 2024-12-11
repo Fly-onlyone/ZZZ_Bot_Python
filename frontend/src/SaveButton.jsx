0c17714d6552b69ac6182da9c51735d8abe871c1
Fly-onlyone <172966293+Fly-onlyone@users.noreply.github.com>
2024-12-11 15:26:43 +0700

    fix: bug Shopping.jsx

---
+++ frontend/src/SaveButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Snackbar, Alert } from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+
+const SaveButton = ({ onSave, alert, setAlert }) => {
+  return (
+    <>
+      <Button
+        className="mt-2 flex gap-2 bg-blue-500 text-white hover:bg-blue-900"
+        variant="contained"
+        color="primary"
+        onClick={onSave}
+      >
+        <SaveIcon />
+        Save
+      </Button>
+      <Snackbar
+        open={alert.open}
+        autoHideDuration={3000}
+        onClose={() => setAlert({ ...alert, open: false })}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <Alert severity={alert.type} variant="outlined">
+          {alert.message}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+};
+
+export default SaveButton;
